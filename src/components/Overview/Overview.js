1fa682ca19f200174099fb4c2e9287c612c84a98
Pawan Kolhe <contact@pawankolhe.com>
2021-02-04 17:09:50 +0530

    Feat: Create fellow dashboard UI

---
+++ src/components/Overview/Overview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Overview.module.scss';
+
+function Overview({ data }) {
+  return (
+      <div className={styles.overviews} style={{
+        gridTemplateColumns: `${'1fr '.repeat(data.length)}`
+      }}>
+        {data.map(item => (
+          <div className={styles.overview}>
+            <div className={styles.overviewText}>{item.value}</div>
+            <div className={styles.overviewValue}>{item.text}</div>
+          </div>
+        ))}
+      </div>
+  );
+}
+
+export default Overview;
