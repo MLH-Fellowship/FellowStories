8a22f55661f9a6f8091791d036764939b4034188
Pawan Kolhe <contact@pawankolhe.com>
2021-02-04 11:58:34 +0530

    Feat HOMEPAGE: Header section UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+
 const features = [
   {
     title: 'Easy to Use',
@@ -59,21 +62,32 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      // title={`Hello from ${siteConfig.title}`}
       description="Platform for MLH Fellows to share their stories and experiences">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
+          <div className={styles.header}>
+            <div className={styles.header__left}>
+              <h1 className={clsx('hero__title', styles.header__title)}><span className="secondarytext">Stories</span> & <span className="secondarytext">Experiences</span><br /> of the MLH Fellowship</h1>
+              <div className={styles.header__buttons}>
+                <Link
+                  className={clsx(
+                    'fs-button fs-button-primary'
+                  )}
+                  to={useBaseUrl('stories/')}>
+                  Read
+                </Link>
+                <Link
+                  className={clsx(
+                    'fs-button fs-button-secondary'
+                  )}
+                  to={useBaseUrl('register/')}>
+                  <FontAwesomeIcon icon={faPen} /><span className="marginleft10">Write Yours</span>
+                </Link>
+              </div>
+            </div>
+            <div className={styles.header__right}>
+              <img className={styles.header__image} src="./img/illustrations/blog-post.svg" alt="image" />
+            </div>
           </div>
         </div>
       </header>
