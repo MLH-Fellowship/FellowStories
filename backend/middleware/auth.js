f538953b9153d4a7d03dc0cac0d561561a66e9fd
Lisa Chandra <liza761c@gmail.com>
2021-02-07 23:39:38 +0530

    feat: delete route

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,31 @@
 // Auth Middleware
-const middlewareObj = {
+const Stories = require('../models/Stories.js')
+
+var middlewareObj = {
 	isLoggedIn: function(req, res, next) {
 		if(req.isAuthenticated()) {
 			return next()
 		}
-		res.redirect("/login")
+		res.redirect('/login')
 	}
-};
+}
+
+middlewareObj.checkStoriesOwnership = function(req, res, next) {
+	if(req.isAuthenticated()) {
+		Stories.findOne({ file_name: req.params.file_name }, function(err, story) {
+			if(err || !story) {
+				res.redirect('Oops, story not found!')
+			} else {
+				if(story.owner_fellow.id.equals(req.user._id)) {
+					next()
+				} else {
+					res.send('You do not have the permission to perform this task!')
+				}
+			}
+		})
+	} else {
+		res.redirect('/login')
+	}
+}
 
 module.exports = middlewareObj
